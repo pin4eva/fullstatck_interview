55f9f62b2402d648131486febb397b241e5ba97f
Peter Akaliro <pin4eva@hotmail.com>
2022-11-20 17:16:13 +0100

    added basic dependencies

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,17 +1,18 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { config } from "dotenv";
+config();
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+  type: "postgres",
+  host: process.env.DATABASE_HOST || "localhost",
+  port: process.env.DATABASE_PORT ? +process.env.DATABASE_PORT : 5432,
+  username: process.env.DATABASE_USERNAME || "test",
+  password: process.env.DATABASE_PASSWORD || "test",
+  database: process.env.DATABASE_NAME || "test",
+  synchronize: process.env.NODE_ENV === "development",
+  logging: process.env.NODE_ENV === "development",
+  entities: ["src/**/*.entity{.ts,.js}"],
+  migrations: ["src/migrations/**/*{.ts,.js}"],
+  subscribers: [],
+});
