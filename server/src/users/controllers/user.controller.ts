5aa960f7a2cd05c0e602c9bb1798372f7e3980e4
Peter Akaliro <pin4eva@hotmail.com>
2022-11-20 17:27:48 +0100

    create user

--- server/src/users/controllers/user.controller.ts
+++ server/src/users/controllers/user.controller.ts
@@ -5,7 +5,21 @@ const router = Router();
 
 const userRepo = AppDataSource.getRepository(User);
 
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
+  try {
+    const user = new User();
+    user.age = +req.body.age;
+    user.firstName = req.body.firstName;
+    user.lastName = req.body.lastName;
+
+    await userRepo.save(user);
+    res.send(user);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+router.get("/", async (_, res) => {
   try {
     const users = await userRepo.find();
     res.send(users);
